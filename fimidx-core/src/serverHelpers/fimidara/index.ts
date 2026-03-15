909b8be82ad3855652490ece6bc250c1c3110731
Abayomi Akintomide <ywordk@gmail.com>
2026-03-15 15:03:42 +0100

    Removed unused code

--- fimidx-core/src/serverHelpers/fimidara/index.ts
+++ fimidx-core/src/serverHelpers/fimidara/index.ts
@@ -1,5 +1,4 @@
 export * from "./downloadFimidaraFile.js";
 export * from "./ensureProjectFimidaraToken.js";
 export * from "./fimidaraClient.js";
-export * from "./listFimidaraFolder.js";
 export * from "./uploadLocalFileToFimidara.js";

3e562e2ca6c4fdc729486cdc1b46548b02c92881
Abayomi Akintomide <ywordk@gmail.com>
2026-04-02 02:06:56 +0100

    Nit changes

--- ixtb-nextjs/vitest.e2e.config.ts
+++ ixtb-nextjs/vitest.e2e.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["api-e2e/**/*.test.ts"],
-    testTimeout: 30000,
-    hookTimeout: 10000,
+    testTimeout: 20_000, // 20 seconds
+    hookTimeout: 10_000, // 10 seconds
     globalSetup: ["./vitest.e2e.setup.ts"],
     fileParallelism: false,
   },

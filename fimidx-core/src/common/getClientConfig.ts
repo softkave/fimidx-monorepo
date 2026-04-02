0953476bbb06d9ef8c7bcaa9019e6c244cd48007
Abayomi Akintomide <ywordk@gmail.com>
2026-04-02 02:02:15 +0100

    Added zod schema for client config

--- fimidx-core/src/common/getClientConfig.ts
+++ fimidx-core/src/common/getClientConfig.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 const publicURL = process.env.NEXT_PUBLIC_URL;
 const fimidxProjectId = process.env.NEXT_PUBLIC_FIMIDX_LOGGER_PROJECT_ID;
 const fimidxClientToken = process.env.NEXT_PUBLIC_FIMIDX_LOGGER_CLIENT_TOKEN;
@@ -6,13 +8,22 @@ const nodeEnv = process.env.NEXT_PUBLIC_PROJECT_ENV;
 const fimidxLoggerEnabled =
   process.env.NEXT_PUBLIC_FIMIDX_LOGGER_ENABLED === "true";
 
+const clientConfigSchema = z.object({
+  publicURL: z.string().url().optional(),
+  fimidxProjectId: z.string(),
+  fimidxClientToken: z.string(),
+  fimidxServerUrl: z.string().url().optional(),
+  nodeEnv: z.string(),
+  fimidxLoggerEnabled: z.boolean().optional().default(false),
+});
+
 export const getClientConfig = () => {
-  return {
+  return clientConfigSchema.parse({
     publicURL,
     fimidxProjectId,
     fimidxClientToken,
     fimidxServerUrl,
     nodeEnv,
     fimidxLoggerEnabled,
-  };
+  });
 };
